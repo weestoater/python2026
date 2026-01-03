d2a066e80c482ce4f94581e6f1a0b773dce3cd69
Ian Burrett <572883+weestoater@users.noreply.github.com>
2026-01-03 09:52:24 +0000

    Initial commit of first chapters

---
+++ python-crash-course/chapter2/remove_prefixes.py
@@ -0,0 +1,4 @@
+nostarch_url = "https://nostarch.com/pythoncrashcourse2e"
+simple_url = nostarch_url.removeprefix("https://")
+print(nostarch_url)
+print(simple_url)
---
+++ python-crash-course/chapter2/whitespaces.py
@@ -0,0 +1,9 @@
+print("Python")
+print("\tPython with a tabbed space\n")
+print("Languages:\nPython\nC\nJavaScript\nCSS\n")
+print("Languages:\n\tPython\n\tC\n\tJavaScript\n\tCSS")
+favorite_language = ' python '
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
